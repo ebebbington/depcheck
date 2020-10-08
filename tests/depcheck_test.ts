bdd442aff19627a03d0083e677503e97354c95e8
Edward Bebbington <edward.bebbington@intercity.technology>
2020-10-08 09:07:26 +0100

    finish tests

--- tests/depcheck_test.ts
+++ tests/depcheck_test.ts
@@ -1,22 +1,14 @@
 import { Rhum } from "./deps.ts";
 import { colours } from "../deps.ts"
 
-async function resetExampleProjectDir() {
-  const p = Deno.run({
-    cmd: [
-      "cp",
-      "-r",
-      "tests/example_project_baseline",
-      "tests/example_project",
-    ],
-  });
-  await p.status();
-  p.close();
+function resetExampleProjectDir() {
+  Deno.copyFileSync("./tests/example_project_baseline/deps.ts", "tests/example_project/deps.ts")
+  Deno.copyFileSync("./tests/example_project_baseline/tests/deps.ts", "tests/example_project/tests/deps.ts")
 }
 
 Rhum.testPlan("tests/depcheck_test.ts", () => {
-  Rhum.beforeEach(async () => {
-    await resetExampleProjectDir();
+  Rhum.afterEach(() => {
+    resetExampleProjectDir();
   });
   Rhum.testSuite("Running with no args", () => {
     Rhum.testCase(
@@ -69,43 +61,58 @@ Rhum.testPlan("tests/depcheck_test.ts", () => {
           colours.yellow('Import "something" is unused, originating from "tests/deps.ts"') + "\n"
       )
       Rhum.asserts.assertEquals(output, colours.green("Cleaned up all unused imports") + "\n")
+      const mainDepsFileContent = new TextDecoder().decode(await Deno.readFile("./tests/example_project/deps.ts"))
+      Rhum.asserts.assertEquals(mainDepsFileContent, "export { heloo } from \"hh\";\n\n")
+      const testDepsFileContent = new TextDecoder().decode(await Deno.readFile("./tests/example_project/tests/deps.ts"))
+      Rhum.asserts.assertEquals(testDepsFileContent, "export { a, b } from \"ffdd\";" + "\n\n" + "export * as anotherThing from \"abc\";\n")
     });
   });
 
-  // Rhum.testSuite("Running with --fmt", () => {
-  //   Rhum.testCase("Formats code beforehand", async () => {
-  //     const p = Deno.run({
-  //       cmd: [
-  //         "deno",
-  //         "run",
-  //         "--allow-read='.'",
-  //         "--allow-run",
-  //         "../../mod.ts",
-  //         "--fmt",
-  //       ],
-  //       cwd: "./tests/example_project",
-  //       stdout: "piped",
-  //     });
-  //     const status = await p.status();
-  //     const output = new TextDecoder().decode(await p.output());
-  //     p.close();
-  //     console.log(output); // todo assert output is the right imports
-  //   });
-  // });
-  //
-  // Rhum.testSuite("User has no dependency files", () => {
-  //   Rhum.testCase("Should do nothing", async () => {
-  //     const p = Deno.run({
-  //       cmd: ["deno", "run", "--allow-read='.'", "../../mod.ts"],
-  //       cwd: "./tests/example_project/src",
-  //       stdout: "piped",
-  //     });
-  //     const status = await p.status();
-  //     const output = new TextDecoder().decode(await p.output());
-  //     p.close();
-  //     console.log(output); // todo assert output is nothing
-  //   });
-  // });
+  Rhum.testSuite("Running with --fmt", () => {
+    Rhum.testCase("Formats code beforehand", async () => {
+      const p = Deno.run({
+        cmd: [
+          "deno",
+          "run",
+          "--allow-read=.",
+          "--allow-run",
+          "../../mod.ts",
+          "--fmt",
+        ],
+        cwd: "./tests/example_project",
+        stdout: "piped",
+        stderr: "piped"
+      });
+      const status = await p.status();
+      const output = new TextDecoder().decode(await p.output());
+      const stderr = new TextDecoder().decode(await p.stderrOutput())
+      p.close();
+      Rhum.asserts.assertEquals(stderr,
+          "Checked 5 files" + "\n" +
+          colours.yellow('Import "byee" is unused, originating from "deps.ts"') + "\n" +
+          colours.yellow('Import "good" is unused, originating from "deps.ts"') + "\n" +
+          colours.yellow('Import "some" is unused, originating from "tests/deps.ts"') + "\n" +
+          colours.yellow('Import "something" is unused, originating from "tests/deps.ts"') + "\n"
+      )
+      Rhum.asserts.assertEquals(output, "")
+    });
+  });
+
+  Rhum.testSuite("User has no dependency files", () => {
+    Rhum.testCase("Should do nothing", async () => {
+      const p = Deno.run({
+        cmd: ["deno", "run", "--allow-read=.", "../../../mod.ts"],
+        cwd: "./tests/example_project/src",
+        stdout: "piped",
+        stderr: "piped"
+      });
+      const status = await p.status();
+      const output = new TextDecoder().decode(await p.output());
+      const stderr = new TextDecoder().decode(await p.stderrOutput())
+      p.close();
+      Rhum.asserts.assertEquals(stderr.indexOf("No such file or directory") > -1, true)
+    });
+  });
 });
 
 Rhum.run()
