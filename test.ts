dae385ba1b4b749350fe2550519ce7fbdeba1020
Edward Bebbington <EdwardSBebbington@hotmail.com>
2022-01-27 13:57:48 +0000

    chore: add coverage

--- test.ts
+++
@@ -1,3 +0,0 @@
-export const Something = {
-  version: import.meta.url,
-};
--- tests/depcheck_test.ts
+++ tests/depcheck_test.ts
@@ -92,7 +92,7 @@ Rhum.testPlan("tests/depcheck_test.ts", () => {
       const stderr = new TextDecoder().decode(await p.stderrOutput());
       p.close();
       Rhum.asserts.assertEquals(
-        stderr.indexOf("No such file or directory") > -1,
+        stderr.includes('system cannot find the file specified'),
         true,
       );
     });
