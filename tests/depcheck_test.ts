b64d2b24779de801389e448778c0c1ef4596a983
Edward Bebbington <EdwardSBebbington@hotmail.com>
2022-01-27 14:57:38 +0000

    tests

--- tests/depcheck_test.ts
+++ tests/depcheck_test.ts
@@ -1,15 +1,15 @@
-import { Rhum } from "./deps.ts";
 import { colours } from "../deps.ts";
+import { assertEquals } from "./deps.ts"
 
 async function warningsAboutUnusedFilesAreCorrect(args: string[]) {
   const { output, stderr, status } = await run(
     args,
     "./tests/example_project",
   );
-  Rhum.asserts.assertEquals(status.success, false);
-  Rhum.asserts.assertEquals(status.code, 1);
-  Rhum.asserts.assertEquals(output, "");
-  Rhum.asserts.assertEquals(
+  assertEquals(status.success, false);
+  assertEquals(status.code, 1);
+  assertEquals(output, "");
+  assertEquals(
     stderr,
     colours.yellow(
       'Import "byee" is unused, originating from "deps.ts"',
@@ -49,54 +49,44 @@ async function run(
   };
 }
 
-Rhum.testPlan("tests/depcheck_test.ts", () => {
-  Rhum.testSuite("Running with no args", () => {
-    Rhum.testCase(
-      "Warnings about unused dependencies are correct",
-      async () => {
-        await warningsAboutUnusedFilesAreCorrect([
-          "deno",
-          "run",
-          "--allow-read=.",
-          "../../mod.ts",
-        ]);
-      },
-    );
-  });
+Deno.test("Running with no args", async t => {
+  await t.step('Warnings about unused dependencies are correct', async () => {
+    await warningsAboutUnusedFilesAreCorrect([
+      "deno",
+      "run",
+      "--allow-read=.",
+      "../../mod.ts",
+    ]);
+  })
+})
 
-  Rhum.testSuite("Running with args", () => {
-    Rhum.testCase(
-      "Warnings about unused dependencies are correct",
-      async () => {
-        await warningsAboutUnusedFilesAreCorrect([
-          "deno",
-          "run",
-          "--allow-read=.",
-          "../../mod.ts",
-          "deps.ts",
-        ]);
-      },
-    );
-  });
+Deno.test("Running with args", async t => {
+  await t.step('Warnings about unused dependencies are correct', async () => {
+    await warningsAboutUnusedFilesAreCorrect([
+      "deno",
+      "run",
+      "--allow-read=.",
+      "../../mod.ts",
+      "deps.ts",
+    ]);
+  })
+})
 
-  Rhum.testSuite("User has no dependency files", () => {
-    Rhum.testCase("Should do nothing", async () => {
-      const p = Deno.run({
-        cmd: ["deno", "run", "--allow-read=.", "../../../mod.ts"],
-        cwd: "./tests/example_project/src",
-        stdout: "piped",
-        stderr: "piped",
-      });
-      await p.status();
-      await p.output();
-      const stderr = new TextDecoder().decode(await p.stderrOutput());
-      p.close();
-      Rhum.asserts.assertEquals(
-        stderr.indexOf("No such file or directory") > -1,
-        true,
-      );
+Deno.test("User has no dependency files", async t => {
+  await t.step('Should do nothing', async () => {
+    const p = Deno.run({
+      cmd: ["deno", "run", "--allow-read=.", "../../../mod.ts"],
+      cwd: "./tests/example_project/src",
+      stdout: "piped",
+      stderr: "piped",
     });
-  });
-});
-
-Rhum.run();
+    await p.status();
+    await p.output();
+    const stderr = new TextDecoder().decode(await p.stderrOutput());
+    p.close();
+    assertEquals(
+      stderr.indexOf("No such file or directory") > -1,
+      true,
+    );
+  })
+})
